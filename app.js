be0fca7d0e3adf2d5aca7ec01e0fde2cee44e096
isarsilva <isasramos12@gmail.com>
2024-11-26 10:33:33 -0300

    att

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá, Mundo!');
+});
+
+
+ap
+listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
